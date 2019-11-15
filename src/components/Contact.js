360dd5360458a267fe2a72e3a2b490b5bfebb625
allan kretzmann <allankretzmann@hotmail.com>
2019-11-15 17:22:05 -0300

    Fix contato

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,12 +11,12 @@ export default class Contact extends Component {
                
                 <div className="contact-info">
                     <p><FontAwesomeIcon icon={faWhatsapp} size="lg"/>: (18) 996241620</p>
-                    <p><FontAwesomeIcon icon={faWhatsapp} size="lg"/>: (18) 996241620</p>
-                    <p><FontAwesomeIcon icon={faWhatsapp} size="lg"/>: (18) 996241620</p>
+                    <p><FontAwesomeIcon icon={faWhatsapp} size="lg"/>: (18) 996184541</p>
+                    <p><FontAwesomeIcon icon={faWhatsapp} size="lg"/>: (18) 997087496</p>
                     <p style={{"margin":"0"}}><a href="https://www.instagram.com/lojinhadatrupe/?hl=pt-br"><FontAwesomeIcon icon={faInstagram} size="3x"/></a></p>
                 </div>
 
-                <img alt="profile" className="profile" src="https://scontent-lga3-1.cdninstagram.com/vp/1dc78d49ffb308fb815bcceb4533d79a/5DCE116F/t51.2885-19/46789637_2305100543110209_8498865277981687808_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com"/>
+                <img alt="profile" className="profile" src="../assets/Profile.jpg"/>
             </div>
         )
     }
